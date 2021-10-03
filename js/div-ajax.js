2f479726d89268ea27f5518adf3bdc204e146e9f
zmzaxg <68527322+zmzaxg@users.noreply.github.com>
2021-10-03 15:36:42 +0800

    Delete div-ajax.js

--- js/div-ajax.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    /* 使用ajax异步提交请求数据，响应数据（回调函数） */
-    $.get("tb/1.html",function (data) {
-        $("#tb1h").html(data);
-    });
-    $.get("nr/1.html",function (data) {
-        $("#nr1h").html(data);
-    });
-});
